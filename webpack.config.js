cf29ae33256820419d0db6167e7d81ef364f451a
704hj <704hj@naver.com>
2024-09-28 16:23:47 +0900

    0928

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,17 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-    mode: "developement",
-    entry: "./src/index.js",
+    entry: "./src/index.js", // 입력 파일
     output: {
-        path: path.resolve(__dirname, "./dist"),
-        filename: "bundle.js",
+        filename: "bundle.js", // 출력 파일
+        path: path.resolve(__dirname, "dist"), // 출력 폴더
     },
-    module: {
-        rules: [
-            {
-                test: /\.(svg|png|jpg|gif|mp4)$/,
-                loader: "file-loader",
-                options: {
-                    name: "assets/[contenthash].[ext]",
-                },
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-            },
-        ],
-    },
-    plugins: [],
+    mode: "development", // development 또는 production
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html", // 원본 HTML 파일 경로
+            filename: "index.html", // 생성될 HTML 파일 이름
+        }),
+    ],
 };
